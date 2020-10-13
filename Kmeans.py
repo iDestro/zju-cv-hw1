f77da4de3b4d5ebd6e0dcf2a64c00a279846a76d
iDestro <573267381@qq.com>
2020-10-13 19:32:01 +0800

    no, no, no, NO,no

--- Kmeans.py
+++ Kmeans.py
@@ -14,17 +14,57 @@ class KMeans:
         n, c = data.shape
         dis = torch.zeros((n, self.n_clusters), device=self.device)
         initial_state = torch.zeros((self.n_clusters, c), device=self.device)
-        # 初始化第一个点的选择概率，此时概率均匀
-        pr = np.repeat(1 / n, n)
-        initial_state[0, :] = data[np.random.choice(np.arange(n), p=pr)]
-        # 计算所有点离第一个centers的距离
-        dis[:, 0] = torch.sum((data - initial_state[0, :]) ** 2, dim=1)
+        idx = np.random.randint(0, n)
+        initial_state[0, :] = data[idx]
 
         for k in range(1, self.n_clusters):
-            pr = torch.sum(dis, dim=1) / torch.sum(dis)
-            initial_state[k, :] = data[np.random.choice(np.arange(n), 1, p=pr.to('cpu').numpy())]
-            for center_idx in range(k+1):
+            for center_idx in range(self.n_clusters):
                 dis[:, center_idx] = torch.sum((data - initial_state[center_idx, :]) ** 2, dim=1)
-
+            min_dist, _ = torch.min(dis, dim=1)
+            p = min_dist / torch.sum(min_dist)
+            initial_state[k, :] = data[np.random.choice(np.arange(n), 1, p=p.to('cpu').numpy())]
 
         return initial_state
+
+    @staticmethod
+    def pairwise_distance(x, y):
+        x = x.unsqueeze(dim=1)
+        y = y.unsqueeze(dim=0)
+        dis = (x-y)**2
+        dis = dis.sum(dim=-1).squeeze()
+        return dis
+
+    def fit(self, data):
+        data = data.to(self.device)
+        cluster_centers = self._initial_state(data)
+
+        while True:
+            dis = self.pairwise_distance(data, cluster_centers)
+            labels = torch.argmin(dis, dim=1)
+            cluster_centers_pre = cluster_centers.clone()
+            for i in range(self.n_clusters):
+                cluster_centers[i, :] = torch.mean(data[labels == i], dim=0)
+
+            center_shift = torch.sum(torch.sqrt(torch.sum((cluster_centers - cluster_centers_pre) ** 2, dim=1)))
+            if center_shift ** 2 < self.tol:
+                break
+
+        self._cluster_centers = cluster_centers
+        self._labels = labels
+
+    def predict(self, x):
+        dis = torch.zeros([x.size(0), self.n_clusters])
+
+        for i in range(self.n_clusters):
+            dis[:, i] = torch.sum((x-self._cluster_centers[i, :])**2, dim=1)
+
+        pred = torch.argmin(dis, dim=1)
+        return pred
+
+    @property
+    def labels_(self):
+        return self._labels
+
+    @property
+    def cluster_centers_(self):
+        return self._cluster_centers
--- test.py
+++ test.py
@@ -1,6 +1,41 @@
-import cv2
-import os
+import torch
 import numpy as np
+from Kmeans import KMeans
 
-x = np.load('descriptors.npy')
-print(x.shape)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+if __name__ == '__main__':
+    import numpy as np
+
+    data_1 = np.random.randn(200, 2) + [1, 1]
+    data_2 = np.random.randn(200, 2) + [4, 4]
+    data_3 = np.random.randn(200, 2) + [7, 1]
+    data = np.concatenate((data_1, data_2, data_3), axis=0)
+
+    data = torch.Tensor(data)
+
+    kmeans = KMeans(n_clusters=3, device=device)
+    kmeans.fit(data)
+
+    import matplotlib.pyplot as plt
+
+    plt.clf()
+    plt.scatter(data[:, 0], data[:, 1], alpha=0.5, c=kmeans.labels_)
+    plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], marker='*', c='k')
+    plt.show()
+
+    test_1 = np.random.randn(200, 2) + [1, 1]
+    test_2 = np.random.randn(200, 2) + [4, 4]
+    test_3 = np.random.randn(200, 2) + [7, 1]
+    test_data = np.concatenate((test_1, test_2, test_3), axis=0)
+
+    test_data = torch.Tensor(test_data)
+
+    clus_pred = kmeans.predict(test_data)
+    plt.clf()
+    plt.scatter(test_data[:, 0], test_data[:, 1], alpha=0.5, c=clus_pred)
+    plt.show()
+
+
